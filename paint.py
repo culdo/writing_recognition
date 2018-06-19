7682e1b660e2f3213349c64cbc7d05a74da20660
lab-pc1 <cbc106008@nptu.edu.tw>
2018-06-19 12:47:02 +0800

    Add switch predict-mode feature, remove useless button, improve image pre-processing.

--- paint.py
+++ paint.py
@@ -1,9 +1,8 @@
 from tkinter import *
-from tkinter.colorchooser import askcolor
 from PIL import Image, ImageDraw
 from keras.models import load_model
 import numpy as np
-
+import io
 
 class Paint(object):
 
@@ -11,30 +10,27 @@ class Paint(object):
     DEFAULT_COLOR = 'black'
     width = 600
     height = 600
-    model = load_model("cnn_model.h5")
+
 
     def __init__(self):
+        self.model = load_model("mlp_model.h5")
+        print("使用單層神經網路")
+        self.shape = (1, 784)
         self.root = Tk()
 
-        self.pen_button = Button(self.root, text='pen', command=self.use_pen)
-        self.pen_button.grid(row=0, column=0)
+        self.color_button = Button(self.root, text='換網路', command=self.choose_NN)
+        self.color_button.grid(row=0, column=0)
 
-        self.brush_button = Button(self.root, text='brush', command=self.use_brush)
-        self.brush_button.grid(row=0, column=1)
+        self.eraser_button = Button(self.root, text='預測', command=self.use_predictor)
+        self.eraser_button.grid(row=0, column=1)
 
-        self.color_button = Button(self.root, text='color', command=self.choose_color)
-        self.color_button.grid(row=0, column=2)
-
-        self.eraser_button = Button(self.root, text='預測', command=self.use_eraser)
-        self.eraser_button.grid(row=0, column=3)
-
-        self.choose_size_button = Scale(self.root, from_=25, to=50, orient=HORIZONTAL)
-        self.choose_size_button.grid(row=0, column=4)
+        self.choose_size_button = Scale(self.root, from_=30, to=150, orient=HORIZONTAL)
+        self.choose_size_button.grid(row=0, column=2)
 
         self.c = Canvas(self.root, bg='white', width=self.width, height=self.height)
         self.image1 = Image.new("L", (self.width, self.height))
         self.draw = ImageDraw.Draw(self.image1)
-        self.c.grid(row=1, columnspan=5)
+        self.c.grid(row=1, columnspan=3)
         self.item = None
 
         self.setup()
@@ -46,30 +42,32 @@ class Paint(object):
         self.line_width = self.choose_size_button.get()
         self.color = self.DEFAULT_COLOR
         self.eraser_on = False
-        self.active_button = self.pen_button
+        # self.active_button = self.pen_button
         self.c.bind('<B1-Motion>', self.paint)
         self.c.bind('<ButtonRelease-1>', self.reset)
-        self.c.bind("<Button-3>", self.rightclick)
+        self.c.bind("<Button-3>", self.right_click)
 
-    def use_pen(self):
-        self.activate_button(self.pen_button)
+    def choose_NN(self):
+        if self.shape == (1, 784):
+            print("使用捲積神經網路")
+            self.model = load_model("cnn_model.h5")
+            self.shape = (1, 28, 28, 1)
+        else:
+            print("使用單層神經網路")
+            self.model = load_model("mlp_model.h5")
+            self.shape = (1, 784)
 
-    def use_brush(self):
-        self.activate_button(self.brush_button)
-
-    def choose_color(self):
-        self.eraser_on = False
-        self.color = askcolor(color=self.color)[1]
-
-    def use_eraser(self):
+    def use_predictor(self):
         filename = "my_drawing.jpg"
-        self.image1.save(filename)
-        array = np.array(self.image1.resize((28, 28))).reshape(1, 784)
-        # print(img.shape)
+        # self.image1.save(filename)
+        ps = self.c.postscript(colormode='gray')
+        img = Image.open(io.BytesIO(ps.encode('utf-8')))
+        gray = img.convert('L')
+        bw = gray.point(lambda x: 255 if x < 128 else 0)
+        bw.save(filename)
+        array = np.array(bw.resize((28, 28))).reshape(self.shape)
         ans = self.model.predict(array)[0]
         print(ans.tolist().index(1))
-        # print(self.image1.resize((28, 28)))
-
 
     def activate_button(self, some_button, eraser_mode=False):
         self.active_button.config(relief=RAISED)
@@ -81,20 +79,18 @@ class Paint(object):
         self.line_width = self.choose_size_button.get()
         paint_color = 'white' if self.eraser_on else self.color
         if self.old_x and self.old_y:
-            self.item = self.c.create_line(self.old_x, self.old_y, event.x, event.y,
+            new = event.x, event.y
+            self.item = self.c.create_line(self.old_x, self.old_y, *new,
                                width=self.line_width, fill=paint_color,
                                capstyle=ROUND, smooth=TRUE, splinesteps=36)
-            self.draw.line([self.old_x, self.old_y, event.x, event.y], width=self.line_width, fill=255)
         self.old_x = event.x
         self.old_y = event.y
 
     def reset(self, event):
         self.old_x, self.old_y = None, None
 
-    def rightclick(self, event):
+    def right_click(self, event):
         self.c.delete("all")
-        self.image1 = Image.new("L", (self.width, self.height))
-        self.draw = ImageDraw.Draw(self.image1)
 
 
 if __name__ == '__main__':
