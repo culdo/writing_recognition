UXD improvement, fix array normalization.

paint.py
@@ -1,30 +1,39 @@
+import io
 from tkinter import *
+
+import numpy as np
 from PIL import Image, ImageDraw
 from keras.models import load_model
-import numpy as np
-import io
+
+scale = 12
+cnn = load_model("cnn_model.h5")
+mlp = load_model("mlp_model.h5")
+
 
 class Paint(object):
 
-    DEFAULT_PEN_SIZE = 5.0
-    DEFAULT_COLOR = 'black'
     width = 600
     height = 600
 
-
     def __init__(self):
-        self.model = load_model("mlp_model.h5")
+        self.model = mlp
         print("使用單層神經網路")
         self.shape = (1, 784)
         self.root = Tk()
+        self.root.title("手寫GUI：->單層網路")
 
-        self.color_button = Button(self.root, text='換網路', command=self.choose_NN)
-        self.color_button.grid(row=0, column=0)
+        self.NN_button = Button(self.root, text='換捲積', command=self.choose_NN, font=("Courier", scale))
+        self.NN_button.grid(row=0, column=0)
 
-        self.eraser_button = Button(self.root, text='預測', command=self.use_predictor)
-        self.eraser_button.grid(row=0, column=1)
+        # self.eraser_button = Button(self.root, text='預測', command=self.use_predictor, font=("Courier", scale))
+        # self.eraser_button.grid(row=0, column=1)
 
-        self.choose_size_button = Scale(self.root, from_=30, to=150, orient=HORIZONTAL)
+        self.stringvar = StringVar()
+        self.label = Label(self.root, textvariable=self.stringvar, font=("Courier", 20))
+        self.label.grid(row=0, column=1)
+        self.stringvar.set("預測戳滾輪、筆粗細->")
+
+        self.choose_size_button = Scale(self.root, from_=30, to=150, orient=HORIZONTAL, font=("Courier", scale), length=150, width=20)
         self.choose_size_button.grid(row=0, column=2)
 
         self.c = Canvas(self.root, bg='white', width=self.width, height=self.height)
@@ -39,23 +48,38 @@ class Paint(object):
     def setup(self):
         self.old_x = None
         self.old_y = None
+        self.choose_size_button.set(90)
         self.line_width = self.choose_size_button.get()
-        self.color = self.DEFAULT_COLOR
-        self.eraser_on = False
-        # self.active_button = self.pen_button
+
         self.c.bind('<B1-Motion>', self.paint)
         self.c.bind('<ButtonRelease-1>', self.reset)
         self.c.bind("<Button-3>", self.right_click)
+        self.c.bind("<Button-2>", self.left_click)
+        self.c.bind("<Button-4>", self._on_mousewheel)
+        self.c.bind("<Button-5>", self._on_mousewheel)
+
+    def _on_mousewheel(self, event):
+        count = self.choose_size_button.get()
+        if event.num == 5 or event.delta == -120:
+            self.choose_size_button.set(count - 1)
+        if event.num == 4 or event.delta == 120:
+            self.choose_size_button.set(count + 1)
 
     def choose_NN(self):
         if self.shape == (1, 784):
             print("使用捲積神經網路")
-            self.model = load_model("cnn_model.h5")
+            self.model = cnn
             self.shape = (1, 28, 28, 1)
+            self.NN_button.config(text="換單層")
+            self.root.title("手寫GUI：->捲積網路")
+            self.use_predictor()
         else:
             print("使用單層神經網路")
-            self.model = load_model("mlp_model.h5")
+            self.model = mlp
             self.shape = (1, 784)
+            self.NN_button.config(text="換捲積")
+            self.root.title("手寫GUI：->單層網路")
+            self.use_predictor()
 
     def use_predictor(self):
         filename = "my_drawing.jpg"
@@ -63,21 +87,18 @@ class Paint(object):
         ps = self.c.postscript(colormode='gray')
         img = Image.open(io.BytesIO(ps.encode('utf-8')))
         gray = img.convert('L')
-        bw = gray.point(lambda x: 255 if x < 128 else 0)
+        bw = gray.point(lambda x: 1 if x < 128 else 0)
         bw.save(filename)
         array = np.array(bw.resize((28, 28))).reshape(self.shape)
+        # array = array.astype("float32")
+        # array /= 255
         ans = self.model.predict(array)[0]
-        print(ans.tolist().index(1))
-
-    def activate_button(self, some_button, eraser_mode=False):
-        self.active_button.config(relief=RAISED)
-        some_button.config(relief=SUNKEN)
-        self.active_button = some_button
-        self.eraser_on = eraser_mode
+        print(np.around(ans, decimals=1))
+        self.stringvar.set(np.argmax(ans))
 
     def paint(self, event):
         self.line_width = self.choose_size_button.get()
-        paint_color = 'white' if self.eraser_on else self.color
+        paint_color = 'black'
         if self.old_x and self.old_y:
             new = event.x, event.y
             self.item = self.c.create_line(self.old_x, self.old_y, *new,
@@ -91,7 +112,11 @@ class Paint(object):
 
     def right_click(self, event):
         self.c.delete("all")
+        self.label.config(font=("Courier", 30))
+        self.stringvar.set("我是答案")
 
+    def left_click(self, event):
+        self.use_predictor()
 
 if __name__ == '__main__':
     Paint()